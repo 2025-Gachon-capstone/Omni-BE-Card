2beddb5d15f821e05517a954664cf4d456adf5b8
Habeomsu <hbs5186@naver.com>
2025-04-13 18:38:10 +0900

    Card 테이블 생성

---
+++ src/main/java/org/example/omnibecard/entity/Card.java
@@ -0,0 +1,44 @@
+package org.example.omnibecard.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.omnibecard.entity.base.BaseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Card")
+public class Card extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cardId;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String memberName;
+
+    @Column(nullable = false)
+    private String cardNumber;
+
+    @Column(nullable = false)
+    private String cardPassword;
+
+    @Column(nullable = false)
+    private String securityCode;
+
+    private LocalDateTime expiredDate;
+
+    @PrePersist
+    public void setExpiredDate() {
+        LocalDateTime baseTime = getCreatedAt() != null ? getCreatedAt() : LocalDateTime.now();
+        this.expiredDate = baseTime.plusYears(5);
+    }
+}
---
+++ src/main/java/org/example/omnibecard/repository/CardRepository.java
@@ -0,0 +1,7 @@
+package org.example.omnibecard.repository;
+
+import org.example.omnibecard.entity.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardRepository extends JpaRepository<Card, Long> {
+}
