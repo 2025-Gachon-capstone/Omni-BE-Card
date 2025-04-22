d87d1733dced0b947c6c8aad449c800b7969b5a8
Habeomsu <hbs5186@naver.com>
2025-04-22 19:44:49 +0900

    #29 전체 혜택 가져오기 구현

---
+++ src/main/java/org/example/omnibecard/dto/CardBenefitResDto.java
@@ -0,0 +1,45 @@
+package org.example.omnibecard.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class CardBenefitResDto {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class GetCardBenefit{
+
+        private Long cardBenefitId;
+        private Long benefitId;
+        private String title;
+        private String sponsorName;
+        private Float discountRate;
+        private LocalDate updatedAt;
+        private LocalDate endDate;
+        private String status;
+
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class GetCardBenefitPage{
+
+        List<CardBenefitResDto.GetCardBenefit> cardBenefits;
+        boolean isFirst;
+        boolean isLast;
+        int pageSize;
+        long totalElements;
+
+    }
+
+
+}
--- src/main/java/org/example/omnibecard/service/CardBenefitService.java
+++ src/main/java/org/example/omnibecard/service/CardBenefitService.java
@@ -1,11 +1,14 @@
 package org.example.omnibecard.service;
 
 import org.example.omnibecard.dto.CardBenefitReqDto;
+import org.example.omnibecard.dto.CardBenefitResDto;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface CardBenefitService {
 
     void createCardBenefit(CardBenefitReqDto.CreateCardBenefit cardBenefitDto);
+    CardBenefitResDto.GetCardBenefitPage getCardBenefits(Long memberId, Pageable pageable);
     void syncCardBenefit(List<CardBenefitReqDto.SyncCardBenefit> syncCardBenefitList);
 }
