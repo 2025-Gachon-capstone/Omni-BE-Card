2540ea2e44bb0c969ffd42dd7dbba67bd437e90c
Habeomsu <hbs5186@naver.com>
2025-05-23 18:22:06 +0900

    #53 BCrypt cost factor 8 변경

--- src/main/java/org/example/omnibecard/common/config/EncryptConfig.java
+++ src/main/java/org/example/omnibecard/common/config/EncryptConfig.java
@@ -10,7 +10,7 @@ public class EncryptConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(8);
     }
 
 }
